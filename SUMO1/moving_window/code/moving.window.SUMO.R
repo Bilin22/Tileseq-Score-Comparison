75d722f362f2eb58a3463e698a403cf67b7dcd4e
Bilin22 <nongbilin1022@outlook.com>
2023-07-18 16:04:28 -0400

    update

--- SUMO1/moving_window/code/moving.window.SUMO.R
+++ SUMO1/moving_window/code/moving.window.SUMO.R
@@ -50,8 +50,15 @@ match_df <- merge(map.2023, map.2019, by = "hgvs_pro") %>%
   rename(score.2023 = score.x, se.2023 = se.x, score.2019 = score.y, se.2019 = se.y, 
          mut = mut.x, wt = wt.x, type = type.x) %>% 
   select(hgvs_pro, score.2023, se.2023, score.2019, se.2019, wt, mut, type) %>% 
-  mutate(plotname = ifelse(abs(score.2023 - score.2019 >= 0.4), hgvs_pro, "")) %>% 
+  mutate(plotname = ifelse(abs(score.2023 - score.2019) >= 0.4, hgvs_pro, "")) %>% 
   mutate(position = ifelse(type == "synonymous", substr(hgvs_pro, 6, nchar(hgvs_pro) - 1), 
-                           substr(hgvs_pro, 6, nchar(hgvs_pro) - 3)))
+                           substr(hgvs_pro, 6, nchar(hgvs_pro) - 3))) %>% 
+  mutate(difference = abs(score.2023 - score.2019))
 
+ggplot(match_df, aes(x = position, y = difference, color = type)) +
+  # geom_point(aes(color = type, alpha = 0.8)) +
+  geom_line(aes(group = type)) +
+  scale_color_manual(values = c("grey59", "red2", "chartreuse3")) +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 70))
 
