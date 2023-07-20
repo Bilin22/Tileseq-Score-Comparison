67e4b7cfdbb2fc8eeee2689b4afbe2839d81fffd
Bilin22 <nongbilin1022@outlook.com>
2023-07-20 14:03:24 -0400

    update script

--- SUMO1/moving_window/code/moving.window.SUMO.R
+++ SUMO1/moving_window/code/moving.window.SUMO.R
@@ -42,31 +42,8 @@ df <- match_df %>%
   select(hgvs_pro, score.2023, score.2019, position) %>% 
   mutate(position = as.numeric(position))
 
-
-## moving window -- Jochen's script
-
-#assume we have a match_df.frame `match_df` containing hgvs.pro, fitness.score, and varity.score columns.
-#extract the amino acid positions from the hgvs column:
-
-#also assume we have a window size parameter:
-window.size <- 2
-#iterate over the range of AA positions
-result <- lapply(range(df$position,na.rm=TRUE), function(pos.i) {
-  #find relevant rows (within window size distance of pos.i)
-  relevantRows <- which(abs((df$position) - pos.i) < window.size)
-  #calculate the correlation between scores of the relevant entries
-  rho <- cor(df[relevantRows,c("score.2023","score.2019")],method="spearman")[1,2]
-  return(c(pos=pos.i,rho=rho))
-})
-#result is currently a list of vectors, which we can turn into a matrix for convenience:
-result <- do.call(rbind,result)
-
-#now we can draw that line plot
-plot(result, type="l", xlab="Position", ylab="Spearman's rho")
-
-
 # moving window for fitness score
-ws <- 25
+ws <- 30
 
 new.columns <- t(sapply(df$position, function(v) {
   relevant <- which(abs(df$position - v) < ws / 2)
@@ -77,7 +54,7 @@ new.columns <- t(sapply(df$position, function(v) {
            rho = rho))
 }))
 
-window.df <- as.data.frame(new.columns)
+window.df <- as.data.frame(new.columns) # data frame for plotting
 
 colors <- c("score2023" = "blue3", "score2019" = "red3")
 
@@ -93,93 +70,13 @@ score.window + guides(color = guide_legend(reverse = TRUE))
 # ggsave(filename = "../Tileseq_Scores/SUMO1/moving_window/output/scores.png", dpi = 700,
 #        height = 6, width = 8)
 
-# moving window for spearman rho
+# moving window for spearman's rho
+ggplot(window.df, aes(x = position, y = rho)) +
+  geom_line() +
+  labs(title = "Spearman Correlation Coefficient between 2023 & 2019 Fitness Score of SUMO1",
+       x = "position", y = "Spearman's rho") +
+  theme_bw()
+# ggsave(filename = "../Tileseq_Scores/SUMO1/moving_window/output/spearman.png", dpi = 700,
+#        height = 6, width = 8)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-# par(mar=c(6, 3, 3, 3) + 2)
-# plot.new()
-
-#set axis for fitness scores
-plot.window(xlim = c(min(new.columns[, "position"]), max(new.columns[, "position"])),
-            #change ylim here for left axis fitness
-            ylim = c(0, 1),
-            xlab = "AA position", ylab = "score.2023", main = "Moving Window Analysis")
-#plot fitness scores
-axis(side = 2, ylab = "score.2023", col = "red",col.axis = 'red', las = 2, line = 0)
-lines(new.columns[, "position"], new.columns[, "score2023.mean"], col = "red", type = "l", lwd = 2)
-
-#create new plot for stability, overlaid on the first fitness plot 
-par(new = TRUE)
-#set axis for stability scores
-plot.window(xlim = c(min(new.columns[, "VARIANT"]), max(new.columns[, "VARIANT"])),
-            #change ylime for right axis stability
-            ylim = c(-1, 0),
-            xlab = "AA position", ylab = "Stability", main = "Moving Window Analysis")
-#Plot stability scores
-axis(side = 4, ylab = "Stability", col = "blue", col.axis = 'blue', las = 2, line = 0)
-lines(new.columns[, "VARIANT"], new.columns[, "stability.mean"], type = "l", lwd = 2, col = "blue")
-
-axis(side = 1, xlab = "VARIANT", col = "black", las = 2, line = 0, at = seq(min(new.columns[, 'VARIANT']), max(new.columns[, 'VARIANT']), by = 10))
-box()
-#legend("bottomright", legend = c("Fitness", "Stability"), fill = c("red", "blue"), cex = 1, x.intersp = 0.2)
-title(main = "Moving Window Analysis of Fitness and Stability Scores", cex.main = 1.2, cex.lab = 1.2)
-# Add labels to x and y axes
-xlabel <- expression("AA position")
-ylabel_left <- expression(paste("Mean ", italic("Fitness")))
-ylabel_right <- expression(paste("Mean ", italic("Stability")))
-
-mtext(xlabel, side = 1, line = 3, cex = 1.2, font = 2)
-mtext(ylabel_left, side = 2, line = 3, cex = 1.2, font = 2, col = 'red')
-#mtext does not allow for rotation
-mtext(ylabel_right, side = 4, line = 3, cex = 1.2, font = 2, col = 'blue')
-
-
-#pearson correlation between moving window of fitness scores and stability scores
-stats <- cor.test(new.columns[,'fitness.mean'], new.columns[,'stability.mean'], method = 'pearson')
-
-p_value <- stats$p.value
-correlation <- stats$estimate
-
-
-
-
-
-
-
-# try plotting the score vs. amino acid position
-# ggplot(map.2023, aes(x = position, y = score, color = type)) +
-#   geom_line(aes(group = type)) +
-#   scale_color_manual(values = c("grey59", "red2", "chartreuse3")) +
-#   facet_grid(~ type)+
-#   theme_bw() +
-#   theme(
-#     axis.text.x = element_text(angle = 60, hjust = 1)
-#   )
-
-
-# ggplot(map.2023, aes(x = position, y = score, color = type)) +
-#   # geom_point(aes(color = type, alpha = 0.8)) +
-#   geom_line(aes(group = type)) +
-#   scale_color_manual(values = c("grey59", "red2", "chartreuse3")) +
-#   theme_classic() +
-#   theme(axis.text.x = element_text(angle = 70))
-
-
-# ggplot(match_df, aes(x = position, y = difference, color = type)) +
-#   # geom_point(aes(color = type, alpha = 0.8)) +
-#   geom_line(aes(group = type)) +
-#   scale_color_manual(values = c("grey59", "red2", "chartreuse3")) +
-#   theme_classic() +
-#   theme(axis.text.x = element_text(angle = 70))
