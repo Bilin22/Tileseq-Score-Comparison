8b743ee9bbde74e05b25f6dc4fb5ddaf865ad34d
Bilin22 <nongbilin1022@outlook.com>
2023-07-26 11:45:27 -0400

    QC for MTHFR with A222V background

--- SUMO1/moving_window/code/moving.window.SUMO.R
+++ SUMO1/moving_window/code/moving.window.SUMO.R
@@ -142,7 +142,6 @@ comparison + guides(color = guide_legend(reverse = TRUE))
 
 
 
-
 # 2. moving window for fitness score
 # set the color manually
 colors <- c("score2023" = "darkgreen", "score2019" = "orange")
